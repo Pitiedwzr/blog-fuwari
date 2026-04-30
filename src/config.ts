e6c260b47e069815406130efcd9e8df86cf4f6c8
Pitiedwzr <2629339380@qq.com>
2026-05-01 03:35:12 +1200

    feat: customise config

--- src/config.ts
+++ src/config.ts
@@ -8,10 +8,10 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "yCENzh's Blog",
-	subtitle: "爱你所爱~",
+	title: "Pitiedwzr's Blog",
+	subtitle: "Be Well.",
 	themeColor: {
-		hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 230, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 		forceDarkMode: false, // Force dark mode and hide the light/dark switcher
 	},
@@ -50,18 +50,18 @@ export const navBarConfig: NavBarConfig = {
 			url: "https://github.com/yCENzh", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},*/
-		{
+		/*{
 			name: "Travelling",
 			url: "https://www.travellings.cn/train.html",
 			external: true,
-		},
+		},*/
 	],
 };
 
 export const profileConfig: ProfileConfig = {
 	avatar: "/avatar.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/' 原assets/images/demo-avatar.png,可选https://q2.qlogo.cn/headimg_dl?dst_uin=189563385&spec=0
-	name: "yCENzh",
-	bio: "Protect What You Love./爱你所爱！",
+	name: "Pitiedwzr",
+	bio: "Be Well.",
 	links: [
 		/*{
 			name: "Twitter",
@@ -78,13 +78,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/yCENzh",
-		},
+			url: "https://github.com/Pitiedwzr",
+		},/*
 		{
 			name: "Telegram",
 			icon: "fa6-brands:telegram",
 			url: "https://t.me/yCENzh",
-		},/*
+		},
 		{
 			name: "开往",
 			icon: "fa6-solid:train-subway",
